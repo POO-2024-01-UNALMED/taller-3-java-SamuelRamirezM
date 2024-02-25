d6295713fedf93b72885ab02f437e5dd3c4c075b
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-02-25 10:21:34 -0500

    Primeras clases

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,18 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	public Marca(String marca) {
+		nombre = marca;
+	}
+	
+	public String getMarca() {
+		return nombre;
+	}
+	
+	public void setMarca(String marca) {
+		nombre = marca;
+	}
+	
+}
